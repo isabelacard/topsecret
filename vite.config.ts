5ad0e7fe80fc1a772e452f36a2311c49c084d48d
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-11-26 07:13:58 -0500

    dg

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         port: 3000,
     },
     build: {
-        outDir: "dist",
-        assetsDir: "assets",
+        outDir: "dist/trevo",
+        emptyOutDir: true,
     },
 });
