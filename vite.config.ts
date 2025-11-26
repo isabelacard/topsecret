47e0a590b5a28f88649459fdf5d489f86f6d253b
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-11-26 05:41:10 -0500

    fix: Change base in vite sixth try

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/trevo/",
+    base: "/",
     plugins: [react(), tailwindcss()],
     server: {
         port: 3000,
